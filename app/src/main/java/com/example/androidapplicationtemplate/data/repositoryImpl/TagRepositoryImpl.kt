2493cc2f9554408e76c11dbf3d6e736a51c85db5
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-28 20:43:14 +0530

    Added Response model classes to TagRepository.kt and its impl

---
+++ app/src/main/java/com/example/androidapplicationtemplate/data/repositoryImpl/TagRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.example.androidapplicationtemplate.data.repositoryImpl
+
+import com.example.androidapplicationtemplate.core.network.Resource
+import com.example.androidapplicationtemplate.data.local.localDataSource.SomeLocalDataSource
+import com.example.androidapplicationtemplate.data.models.response.TagListResponse
+import com.example.androidapplicationtemplate.data.remote.remoteDataSource.TagRemoteDataSource
+import com.example.androidapplicationtemplate.domain.repository.TagRepository
+import javax.inject.Inject
+
+class TagRepositoryImpl @Inject constructor(
+    private val localDataSource: SomeLocalDataSource,
+    private val remoteDataSource: TagRemoteDataSource,
+) : TagRepository {
+
+    override suspend fun someCrudOperation(): Resource<TagListResponse> {
+        return Resource.Success(remoteDataSource.getTags())
+    }
+
+}
