9506c93d49b570916b5559468270496896501c25
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-29 05:24:39 +0530

    Chores

--- app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteServices/TagServices.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteServices/TagServices.kt
@@ -1,13 +1,12 @@
 package com.example.androidapplicationtemplate.data.remote.remoteServices
 
 import com.example.androidapplicationtemplate.core.credentials.API_KEY
-import com.example.androidapplicationtemplate.data.models.response.Response
 import com.example.androidapplicationtemplate.data.models.response.TagListResponse
 import retrofit2.http.GET
 
 interface TagServices {
 
     @GET("/2.0/?method=tag.gettoptags&api_key=${API_KEY}&format=json")
-    suspend fun getTags(): TagListResponse
+    suspend fun getAllTags(): TagListResponse
 
 }
--- app/src/main/java/com/example/androidapplicationtemplate/domain/repository/TagRepository.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/domain/repository/TagRepository.kt
@@ -1,9 +1,8 @@
 package com.example.androidapplicationtemplate.domain.repository
 
 import com.example.androidapplicationtemplate.core.network.Resource
-import com.example.androidapplicationtemplate.data.models.response.Response
 import com.example.androidapplicationtemplate.data.models.response.TagListResponse
 
 interface TagRepository {
-    suspend fun someCrudOperation(): Resource<TagListResponse>
+    suspend fun getAllTags(): Resource<TagListResponse>
 }
