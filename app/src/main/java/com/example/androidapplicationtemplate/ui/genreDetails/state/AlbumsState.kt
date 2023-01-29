66f71f6ff4635e5b7b954094c14fcccfc1a49c39
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-29 13:52:08 +0530

    UI to show tag details to tag

--- app/src/main/java/com/example/androidapplicationtemplate/ui/genreDetails/state/AlbumsState.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/ui/genreDetails/state/AlbumsState.kt
@@ -1,8 +1,11 @@
 package com.example.androidapplicationtemplate.ui.genreDetails.state
 
+import com.example.androidapplicationtemplate.data.models.response.Album
+
 sealed class AlbumsState{
     object Idle : AlbumsState()
     object Loading : AlbumsState()
     object Error : AlbumsState()
     object State1 : AlbumsState()
+    data class ArgumentsProcessed(val album: Album) : AlbumsState()
 }
