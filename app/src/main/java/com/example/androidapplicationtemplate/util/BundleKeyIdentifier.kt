f1c7d5194b547f3b70efb2e71fd8254aa776aade
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-29 07:37:36 +0530

    refactor - CHORES

--- app/src/main/java/com/example/androidapplicationtemplate/util/BundleKeyIdentifier.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/util/BundleKeyIdentifier.kt
@@ -5,4 +5,8 @@ object BundleKeyIdentifier {
     const val SEARCH_QUERY = "SEARCH_QUERY"
     const val WIKI_DATA = "WIKI_DATA"
 
+    var POSITION_ARG = "position_arg"
+    var TAG = "category_id"
+
+
 }
