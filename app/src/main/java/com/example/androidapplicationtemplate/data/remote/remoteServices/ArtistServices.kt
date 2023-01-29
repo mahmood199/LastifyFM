fec95e9b293487b2b0b4438139cb65d9dbe89a40
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-29 06:04:51 +0530

    Artist bugfix

--- app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteServices/ArtistServices.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteServices/ArtistServices.kt
@@ -2,12 +2,13 @@ package com.example.androidapplicationtemplate.data.remote.remoteServices
 
 import com.example.androidapplicationtemplate.core.credentials.API_KEY
 import com.example.androidapplicationtemplate.data.models.response.AlbumListResponse
+import com.example.androidapplicationtemplate.data.models.response.ArtistListResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ArtistServices {
 
     @GET("/2.0/?method=tag.gettopartists&api_key=${API_KEY}&format=json")
-    suspend fun getArtistsByTag(@Query("tag") tag: String): AlbumListResponse
+    suspend fun getArtistsByTag(@Query("tag") tag: String): ArtistListResponse
 
 }
--- app/src/main/java/com/example/androidapplicationtemplate/domain/usecase/GetTopArtistsByTagUseCase.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/domain/usecase/GetTopArtistsByTagUseCase.kt
@@ -2,8 +2,9 @@ package com.example.androidapplicationtemplate.domain.usecase
 
 import com.example.androidapplicationtemplate.core.network.Resource
 import com.example.androidapplicationtemplate.data.models.response.AlbumListResponse
+import com.example.androidapplicationtemplate.data.models.response.ArtistListResponse
 import com.example.androidapplicationtemplate.data.models.response.Tag
-import com.example.androidapplicationtemplate.domain.repository.AlbumRepository
+import com.example.androidapplicationtemplate.domain.repository.ArtistRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -11,12 +12,12 @@ import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetTopArtistsByTagUseCase @Inject constructor(
-    private val albumRepository: AlbumRepository
+    private val artistRepository: ArtistRepository
 ) {
 
-    suspend operator fun invoke(tag: Tag): Flow<Resource<AlbumListResponse>> = flow {
+    suspend operator fun invoke(tag: Tag): Flow<Resource<ArtistListResponse>> = flow {
         emit(Resource.Loading)
-        val result = albumRepository.getAlbumsByTag(tag)
+        val result = artistRepository.getArtistByTag(tag)
         emit(result)
     }.flowOn(Dispatchers.IO)
 
