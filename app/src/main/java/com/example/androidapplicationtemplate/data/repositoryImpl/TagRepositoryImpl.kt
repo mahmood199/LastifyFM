60465f7f2216786675dc07cd1fe45b95bdccff07
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-29 06:23:25 +0530

    data layer changes to add new api

--- app/src/main/java/com/example/androidapplicationtemplate/data/repositoryImpl/TagRepositoryImpl.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/repositoryImpl/TagRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.androidapplicationtemplate.data.repositoryImpl
 
 import com.example.androidapplicationtemplate.core.network.Resource
 import com.example.androidapplicationtemplate.data.local.localDataSource.SomeLocalDataSource
+import com.example.androidapplicationtemplate.data.models.response.Tag
 import com.example.androidapplicationtemplate.data.models.response.TagListResponse
 import com.example.androidapplicationtemplate.data.remote.remoteDataSource.TagRemoteDataSource
 import com.example.androidapplicationtemplate.domain.repository.TagRepository
@@ -13,7 +14,11 @@ class TagRepositoryImpl @Inject constructor(
 ) : TagRepository {
 
     override suspend fun getAllTags(): Resource<TagListResponse> {
-        return Resource.Success(remoteDataSource.getAllTags())
+        return remoteDataSource.getAllTags()
+    }
+
+    override suspend fun getTagInfo(tag: String): Resource<Tag> {
+        return remoteDataSource.getTagInfo(tag)
     }
 
 }
