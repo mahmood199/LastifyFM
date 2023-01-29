2c2774f38216682c92155b764af2dbb839fcb0cb
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-29 16:17:33 +0530

    Making models implement parcelable interface to send across screen

--- app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Streamable.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Streamable.kt
@@ -1,10 +1,13 @@
 package com.example.androidapplicationtemplate.data.models.response
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Streamable(
     @SerializedName("#text")
     val text: String,
     @SerializedName("fulltrack")
     val fullTrack: String
-)
+) : Parcelable
