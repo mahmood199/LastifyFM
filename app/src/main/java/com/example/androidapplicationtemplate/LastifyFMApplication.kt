19ef36ac758106c8679a0e887dd077f52daf5388
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-29 18:41:39 +0530

    TagViewHOlder added to GenericAdapter.kt

--- app/src/main/java/com/example/androidapplicationtemplate/LastifyFMApplication.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/LastifyFMApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TemplateApplication : Application() {
+class LastifyFMApplication : Application() {
 
 	override fun onCreate() {
 		super.onCreate()
